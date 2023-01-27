63019948782edee44641802aa21509ec1e9403d7
Oğuzhan Cihan <oguzhan.cihan@ekinokssoftware.com>
2023-01-27 21:59:54 +0300

    orderapp

--- HomeTypeApp/src/entites/Summerhouse.java
+++
@@ -1,7 +0,0 @@
-package entites;
-
-public class Summerhouse extends House{
-    public Summerhouse(Long id, String houseType, double price, double squareMeters, int numberOfRooms, int numberOfHalls) {
-        super(id, houseType, price, squareMeters, numberOfRooms, numberOfHalls);
-    }
-}
